1dc8ed3f22e648f05da11776f3949d4d08b21f81
eimea <polic@majster.hub.pl>
2021-11-17 23:53:18 +0100

    update

--- konta/admin.py
+++ konta/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Gracze
+
+admin.site.register(Gracze)
 # Register your models here.
--- konta/models.py
+++ konta/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Gracze(models.Model):
+
+    nazwa = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True)
+    data = models.DateTimeField(auto_now_add=True, null=True)
+
+
