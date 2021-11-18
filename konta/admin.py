2914c07382942f23369d8025e427fa426d2b8683
eimea <polic@majster.hub.pl>
2021-11-18 23:44:49 +0100

    update

--- konta/admin.py
+++ konta/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Gracze
+from .models import Gracze, BlogPost
 
 admin.site.register(Gracze)
+admin.site.register(BlogPost)
 # Register your models here.
--- konta/models.py
+++ konta/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+
 class Gracze(models.Model):
 
     nazwa = models.CharField(max_length=50, null=True)
     email = models.CharField(max_length=50, null=True)
     data = models.DateTimeField(auto_now_add=True, null=True)
 
+    def __str__(self):
+        return self.nazwa
 
+
+class BlogPost(models.Model):
+    tytul = models.CharField(max_length=150, null=True)
+    tresc = models.TextField()
+    data_utw = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.tytul
