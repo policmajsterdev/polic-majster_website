db2b684d6e38bf770b8637493a9b1d8449cb9cee
eimea <polic@majster.hub.pl>
2021-11-20 00:40:02 +0100

    update

--- konta/urls.py
+++ konta/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('kontakt/', views.kontakt),
-    path('omnie/', views.omnie),
+    path('', views.home, name="home"),
+    path('kontakt/<int:my_id>/', views.kontakt, name="kontakt"),
+    path('omnie/', views.omnie, name="omnie"),
 ]
--- konta/views.py
+++ konta/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 
 def home(request):
+
     return render(request, 'konta/dash.html')
 
-def kontakt(request):
-    return render(request, 'konta/kontakt.html')
+
+def kontakt(request, my_id):
+    strona = BlogPost.objects.get(id=my_id)
+    context = {'strona': strona}
+    return render(request, 'konta/kontakt.html', context)
+
 
 def omnie(request):
-    return render(request, 'konta/omnie.html')
-
-
-
-# Create your views here.
+    posty = BlogPost.objects.all()
+    total_posts = posty.count()
+    context = {'posty': posty, 'total_posts': total_posts}
+    return render(request, 'konta/omnie.html', context)
